4d54db0dd2b9251fe86d4fb2c8f2d83778ffc817
litospayaso <angel.jesuita@gmail.com>
2015-12-27 00:48:55 +0100

    icon changed and pulse funcionality added

--- app/js/provider/pulseProvider.js
+++ app/js/provider/pulseProvider.js
@@ -3,19 +3,43 @@ angular.module("gessami")
     .provider("PulseProvider", function () {
         "use strict";
 
+        var currentPulse;
+
         this.$get = function () {
             return {
                 getPulseData: function (successHandler, errorHandler) {
-                    var pulso = Math.round(Math.random() * 10),
-                        alerta = 6;
-
-                    if (Math.random() < 0.95) {
-                        successHandler({
-                            pulso: pulso,
-                            alerta: alerta
+                    var aceleracionAnterior;
+                    if (navigator && navigator.accelerometer && navigator.accelerometer.watchAcceleration) {
+                        currentPulse = navigator.accelerometer.watchAcceleration(function (acceleration) {
+                            var aceleracionActual;
+                            if (aceleracionAnterior !== undefined) {
+                                aceleracionActual = {};
+                                aceleracionActual.x = Math.abs(aceleracionAnterior.x - acceleration.x);
+                                aceleracionActual.y = Math.abs(aceleracionAnterior.y - acceleration.y);
+                                aceleracionActual.z = Math.abs(aceleracionAnterior.z - acceleration.z);
+                                aceleracionActual.total = aceleracionActual.x + aceleracionActual.y + aceleracionActual.z;
+                                aceleracionAnterior = acceleration;
+                                successHandler({
+                                    pulso: aceleracionActual.total,
+                                    alerta: 20
+                                });
+                            }
+                            aceleracionAnterior = acceleration;
+                        }, function (error) {
+                            errorHandler("Pulse error: " + error.code + " - " + error.message);
+                        }, {
+                            maximumAge: 3000,
+                            timeout: 30000,
+                            enableHighAccuracy: true
                         });
                     } else {
-                        errorHandler("Error when getting pulse");
+                        errorHandler("Accelerometer capability is not available");
+                    }
+                },
+
+                freePulse: function () {
+                    if (currentPulse) {
+                        navigator.accelerometer.clearWatch(currentPulse);
                     }
                 }
             };
